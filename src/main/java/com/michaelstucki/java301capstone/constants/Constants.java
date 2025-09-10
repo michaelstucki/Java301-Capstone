052beca208301f91b5b35d53e098f283be4581f8
Michael Stucki <stcuthbertus@gmail.com>
2025-09-09 19:08:22 -0600

    adjust window size

--- src/main/java/com/michaelstucki/java301capstone/constants/Constants.java
+++ src/main/java/com/michaelstucki/java301capstone/constants/Constants.java
@@ -4,7 +4,7 @@ import com.michaelstucki.java301capstone.util.SceneManager;
 
 public final class Constants {
     private static final double ASPECT_RATIO = 1.0;
-    public static final double width = 850;
+    public static final double width = 750;
     public static final double height = width * ASPECT_RATIO;
     public static final String databasePath = "src/main/resources/database/test.db";
     public static final String usersTable = "users";
