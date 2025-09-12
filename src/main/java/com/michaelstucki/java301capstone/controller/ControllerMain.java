7e18bd01393b843ee25b2b01c54dc71fe4c39004
Michael Stucki <stcuthbertus@gmail.com>
2025-09-12 10:21:53 -0600

    crop trieme, add custon cellfactory in home controller initialize to enable acting on items in viewlist, implement delete item in viewlist

--- src/main/java/com/michaelstucki/java301capstone/controller/ControllerMain.java
+++ src/main/java/com/michaelstucki/java301capstone/controller/ControllerMain.java
@@ -1,24 +1,63 @@
 package com.michaelstucki.java301capstone.controller;
 
+import com.michaelstucki.java301capstone.util.SceneManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.util.Callback;
 
 public class ControllerMain {
     @FXML
+    private ContextMenu itemContextMenu;
+    @FXML
     private ListView<String> decksView;
+    private SceneManager sceneManager;
+    private ObservableList<String> items;
 
     @FXML
     public void initialize() {
-        ObservableList<String> items = FXCollections.observableArrayList("A", "B", "C", "D", "E", "F", "G", "H",
+        sceneManager = SceneManager.getScreenManager();
+
+        items = FXCollections.observableArrayList("A", "B", "C", "D", "E", "F", "G", "H",
             "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"
                 );
         decksView.setItems(items);
-        decksView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                System.out.println("Selected item:" + newValue);
+        decksView.setCellFactory(new Callback<>() {
+            @Override
+            public ListCell<String> call(ListView<String> listView) {
+                return new ListCell<>() {
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty || item == null) {
+                            setText(null);
+                            setGraphic(null);
+                            setContextMenu(null);
+                        } else {
+                            setText(item);
+                            setContextMenu(itemContextMenu);
+                        }
+                    }
+                };
             }
         });
+    }
+
+    public void open(ActionEvent event) {
+        System.out.println("open deck");
+    }
+
+    public void delete(ActionEvent event) {
+        String selectedItem = decksView.getSelectionModel().getSelectedItem();
+        decksView.getItems().remove(selectedItem);
+    }
+
+    public void exitClick() {
+        sceneManager.exit();
     }
 }
