49b71450769283982b2e0d7680f38f1a2f2707e0
Michael Stucki <stcuthbertus@gmail.com>
2025-09-14 09:28:50 -0600

    add sqlite dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.michaelstucki.java301capstone {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.sql;
 
 
     opens com.michaelstucki.java301capstone to javafx.fxml;
