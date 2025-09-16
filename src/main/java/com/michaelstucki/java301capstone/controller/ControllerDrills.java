0e9c7d2986968fdcc7e0a12cf7dde84c2e49c2fb
Michael Stucki <stcuthbertus@gmail.com>
2025-09-16 00:57:12 -0600

    add all drills UI buttons and code their logic in controller and the event-driven looping logic over mocked set of cards using a simple counter

--- src/main/java/com/michaelstucki/java301capstone/controller/ControllerDrills.java
+++ src/main/java/com/michaelstucki/java301capstone/controller/ControllerDrills.java
@@ -8,6 +8,16 @@ import javafx.scene.control.*;
 
 public class ControllerDrills {
     @FXML
+    private Button start;
+    @FXML
+    private Button stop;
+    @FXML
+    private Button pass;
+    @FXML
+    private Button next;
+    @FXML
+    private Button fail;
+    @FXML
     private TextArea questionAnswer;
     @FXML
     private Label deckName;
@@ -15,20 +25,43 @@ public class ControllerDrills {
     private Deck deck;
     private boolean isFront;
 
+    // Demo items
+    private final int totalIterations = 2;
+    private int currentIteration = 0;
+
     public void init(Deck sharedDeck) {
         deck = sharedDeck;
         deckName.setText(deck.getName());
+        start.setDisable(false);
+        stop.setDisable(true);
+        next.setDisable(true);
+        pass.setDisable(true);
+        fail.setDisable(true);
+
+        // Demo
+        currentIteration = 0;
     }
 
-    private void drill() {
+    public void startClick(ActionEvent event) {
+        start.setDisable(true);
+        stop.setDisable(false);
+        next.setDisable(false);
+    }
+
+    public void stopClick(ActionEvent event) {
+        init(deck);
     }
 
     public void passClick(ActionEvent event) {
         System.out.println("passClick");
+        pass.setDisable(true);
+        fail.setDisable(true);
     }
 
     public void failClick(ActionEvent event) {
         System.out.println("failClick");
+        pass.setDisable(true);
+        fail.setDisable(true);
     }
 
     public void decksClick() {
@@ -43,14 +76,27 @@ public class ControllerDrills {
     public void initialize() {
         sceneManager = SceneManager.getScreenManager();
         questionAnswer.setEditable(false);
+
+        // Demo items
         isFront = true;
         String front = "front";
         String back = "back";
         questionAnswer.setText(front);
-
         questionAnswer.setOnMouseClicked(event -> {
             questionAnswer.setText(isFront ? back : front);
             isFront = !isFront;
+        });
+
+        next.setOnAction(event -> {
+            pass.setDisable(false);
+            fail.setDisable(false);
+            if (currentIteration < totalIterations) {
+                System.out.println("currentIteration: " + currentIteration);
+                currentIteration++;
+                if (currentIteration == totalIterations) {
+                    init(deck);
+                }
+            }
         });
     }
 }
